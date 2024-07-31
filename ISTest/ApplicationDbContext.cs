9c88a728fc8d1a4076a1160ebdaa9fc218a80aa5
Mark Daniel Louwe <mrkdnlph@gmail.com>
2024-07-31 15:05:02 +0800

    Add project files.

---
+++ ISTest/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Reflection.Emit;
+
+namespace ISTest
+{
+    public class ApplicationDbContext
+    {
+    }
+}
+
+//+++++++++++
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISTest
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
